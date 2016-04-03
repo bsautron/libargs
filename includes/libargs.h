7260fe9d04c51d8fcbdfc1e3a0283c2c2e191f91
Bruno SAUTRON <bsautron@e2r4p18.42.fr>
2016-04-03 19:23:03 +0200

    header normed

--- includes/libargs.h
+++ includes/libargs.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libargs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/03 19:21:56 by bsautron          #+#    #+#             */
+/*   Updated: 2016/04/03 19:22:50 by bsautron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBARGS_H
 # define LIBARGS_H
 
@@ -5,67 +17,67 @@
 # include <liblist.h>
 # include <libhtab.h>
 
-# define	ARGS_NAME_LEN_MAX		255
-# define	ARGS_USAGE_LEN_MAX		1024
+# define ARGS_NAME_LEN_MAX		255
+# define ARGS_USAGE_LEN_MAX		1024
 
-typedef struct  s_man_option
+typedef struct	s_man_option
 {
-	char      brief;
-	char      *large;
-	char      *desc;
-}               t_man_option;
+	char	brief;
+	char	*large;
+	char	*desc;
+}				t_man_option;
 
-typedef struct  s_list_man_option
+typedef struct	s_list_man_option
 {
-	void          *next;
-	char          *key;
-	t_man_option  *man;
-}               t_list_man_option;
+	void			*next;
+	char			*key;
+	t_man_option	*man;
+}				t_list_man_option;
 
 typedef	struct	s_commander_desc
 {
-	void 		*next;
-	char		*value;
+	void	*next;
+	char	*value;
 }				t_commander_desc;
 
-typedef struct  s_args
+typedef struct	s_args
 {
 	char				name[ARGS_NAME_LEN_MAX];
 	char				usage[ARGS_USAGE_LEN_MAX];
 	t_commander_desc	*desc;
 	char				**examples;
 	t_list_man_option	*mans;
-	t_htab           	options;
-	int              	argc;
-	char              	**argv;
-}               t_args;
+	t_htab				options;
+	int					argc;
+	char				**argv;
+}				t_args;
 
-typedef struct  s_options_value
+typedef struct	s_options_value
 {
-	void    *next;
-	char    *value;
-}               t_options_value;
+	void	*next;
+	char	*value;
+}				t_options_value;
 
-typedef struct  s_option
+typedef struct	s_option
 {
-	void            *next;
-	char            *key;
-	int             set;
-	int             nb_values;
-	t_man_option    *man;
-	t_options_value *values;
-}               t_option;
+	void			*next;
+	char			*key;
+	int				set;
+	int				nb_values;
+	t_man_option	*man;
+	t_options_value	*values;
+}				t_option;
 
-void      init_options(t_args *args, char *name);
-void      add_option(t_args *args, char *brief, char *large, char *desc);
-int      parse_options(t_args *args, int argc, char const **argv);
-t_option  *get_option(t_args args, char *key);
-void      show_args(t_args args);
-void 		show_usage(t_args args);
-int       option_is_set(t_args args, char *key);
-char      **get_values_option(t_args args, char *key);
-void 		add_desc(t_args *args, char *desc);
-void 		add_usage(t_args *args, char *usage);
+void			init_options(t_args *args, char *name);
+void			add_option(t_args *args, char *brief, char *large, char *desc);
+int				parse_options(t_args *args, int argc, char const **argv);
+t_option		*get_option(t_args args, char *key);
+void			show_args(t_args args);
+void			show_usage(t_args args);
+int				option_is_set(t_args args, char *key);
+char			**get_values_option(t_args args, char *key);
+void			add_desc(t_args *args, char *desc);
+void			add_usage(t_args *args, char *usage);
 int				treat_option(t_option *option, char const *argv);
 
 #endif
